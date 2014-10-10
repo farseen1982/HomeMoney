6a676253cafcc58369b28d730085c70bdfb80097
farseen1982 <farseen1982@gmail.com>
2014-10-10 17:50:33 +0800

    office: add text

--- HomeMoney/src/com/farseen/homemoney/DetailActivity.java
+++ HomeMoney/src/com/farseen/homemoney/DetailActivity.java
@@ -7,12 +7,14 @@ import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class DetailActivity extends Activity {
 	OnClickListener btnHandeler;
 	Button btnSave;
 	Button btnCancal;
 	Button btnShowList;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -20,36 +22,42 @@ public class DetailActivity extends Activity {
 		setListener();
 	}
 
-	
-
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.detail, menu);
 		return true;
 	}
-	
+
 	private void setListener() {
-		btnHandeler = new OnClickListener(){
-		public void onClick(View view) {
+		btnHandeler = new OnClickListener() {
+			public void onClick(View view) {
 				int id = view.getId();
-				switch(id){
+				switch (id) {
 				case R.id.btn_journal:
 					navJournal();
+				case R.id.btn_entry:
+					insertJournal();
 				}
 			}
 
 		};
 		btnShowList = (Button) this.findViewById(R.id.btn_journal);
 		btnShowList.setOnClickListener(btnHandeler);
-		
+
 	}
+
 	private void navJournal() {
 		Intent intent = new Intent(DetailActivity.this, JournalActivity.class);
 		this.startActivity(intent);
-		
+
 	}
-		
-	
+
+	private void insertJournal() {
+		EditText date = (EditText)this.findViewById(R.id.edit_date);
+		EditText amount = (EditText)this.findViewById(R.id.edit_exp);
+		EditText member = (EditText)this.findViewById(R.id.edit_member);
+
+	}
 
 }
---
+++ HomeMoney/src/com/farseen/homemoney/HomeMoneyConst.java
@@ -0,0 +1,9 @@
+package com.farseen.homemoney;
+
+public class HomeMoneyConst {
+	public static String TAG = "HomeMoney";
+	public HomeMoneyConst() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
